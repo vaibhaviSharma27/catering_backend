75e5b8374c15f69685bdc4c049515280e25d8215
Vaibhavi <vaibhavisharma.sv2527@gmail.com>
2026-07-05 22:52:29 +0530

    asdas

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import "dotenv/config";
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"catering-frontend-self.vercel.app",
     credentials: true
 }));
 
